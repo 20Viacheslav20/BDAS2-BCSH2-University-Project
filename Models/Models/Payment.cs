d40bd19f4a1ede3531b1af9f13f9a3d8f2a1643d
Mykhailo Onipchenko <95632067+M1SHANIA@users.noreply.github.com>
2023-12-03 22:19:58 +0100

    payment

---
+++ Models/Models/Payment.cs
@@ -0,0 +1,23 @@
+﻿namespace Models.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public bool isClubCard { get; set; }
+    }
+
+    public class Coupon : Payment
+    {
+        public int Number { get; set; }
+    }
+
+    public class Cash : Payment
+    {
+        public int Returned { get; set; }
+    }
+
+    public class CreditCard : Payment
+    {
+        public int CardNumber { get; set; }
+    }
+}
---
+++ Repositories/Repositories/PaymentRepository.cs
@@ -0,0 +1,43 @@
+﻿using Models.Models;
+using Oracle.ManagedDataAccess.Client;
+using Repositories.IRepositories;
+
+namespace Repositories.Repositories
+{
+    public class PaymentRepository : IPaymentRepository
+    {
+        private readonly OracleConnection _oracleConnection;
+
+        private const string TABLE = "PLATBY";
+
+        public PaymentRepository(OracleConnection oracleConnection)
+        {
+            _oracleConnection = oracleConnection;
+        }
+
+        public void Create(Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Payment> GetAllPayments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Payment GetPayment(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
